07a0c1b1da05c119dcec83717d90085c3e0b8ce3
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-06-23 22:02:21 +0300

    add Navbar and NotFound components to App for improved navigation and 404 handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,21 @@ import AboutPage from "./pages/About";
 import GamesPage from "./pages/Games";
 import HomePage from "./pages/Index";
 import GameDetails from "./pages/GameDetails";
+import Navbar from "./layout/Navbar";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/games" element={<GamesPage />} />
         <Route path="/game/:id" element={<GameDetails />} />
         <Route path="/about" element={<AboutPage />} />
+
+        {/* Fallback route for 404 Not Found */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
