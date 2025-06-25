e9206bb95f241248579b4b087373dcb920d1af58
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-06-25 06:16:14 +0300

    refactor routing structure to use AppLayout for better organization and add AppLayout component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,25 +3,27 @@ import AboutPage from "./pages/About";
 import GamesPage from "./pages/Games";
 import HomePage from "./pages/Index";
 import GameDetails from "./pages/GameDetails";
-import Navbar from "./layout/Navbar";
 import NotFound from "./pages/NotFound";
 import LoginPage from "./pages/Login";
 import { Toaster } from "./components/ui/toaster";
+import AppLayout from "./layout/AppLayout";
 
 function App() {
   return (
     <>
       <Toaster />
-      <Navbar />
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/games" element={<GamesPage />} />
-        <Route path="/game/:id" element={<GameDetails />} />
-        <Route path="/about" element={<AboutPage />} />
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/games" element={<GamesPage />} />
+          <Route path="/game/:id" element={<GameDetails />} />
+          <Route path="/about" element={<AboutPage />} />
 
-        {/* Fallback route for 404 Not Found */}
-        <Route path="*" element={<NotFound />} />
+          {/* Fallback route for 404 Not Found */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+        
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
   );
