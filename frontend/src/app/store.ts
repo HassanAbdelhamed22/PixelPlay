6821a1e4b16aad3d5b23f843e88b5ac2ca829271
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-07-02 07:20:28 +0300

    integrate apiSlice into Redux store for enhanced data management

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginSlice from "./features/loginSlice";
 import cartSlice from "./features/cartSlice";
+import { apiSlice } from "./services/apiSlice";
 
 // Define the root state type
 export type RootState = ReturnType<typeof store.getState>;
@@ -12,5 +13,10 @@ export const store = configureStore({
   reducer: {
     login: loginSlice,
     cart: cartSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Disable serializable check for non-serializable data
+    }).concat(apiSlice.middleware),
 });
