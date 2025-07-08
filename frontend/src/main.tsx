f9ecff6f2066c6aee467f658ae515d6ed4bb49a3
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-07-08 21:55:53 +0300

    wrap all component with  InternetConnectionProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./app/store.ts";
 import AuthInitializer from "./components/AuthInitializer.tsx";
+import InternetConnectionProvider from "./provider/InternetConnectionProvider.tsx";
 
 // Create a client
 const queryClient = new QueryClient({
@@ -23,14 +24,16 @@ const queryClient = new QueryClient({
 });
 
 createRoot(document.getElementById("root")!).render(
-  <ReduxProvider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Provider>
-          <AuthInitializer />
-          <App />
-        </Provider>
-      </Router>
-    </QueryClientProvider>
-  </ReduxProvider>
+  <InternetConnectionProvider>
+    <ReduxProvider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Provider>
+            <AuthInitializer />
+            <App />
+          </Provider>
+        </Router>
+      </QueryClientProvider>
+    </ReduxProvider>
+  </InternetConnectionProvider>
 );
