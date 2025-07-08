cc503be52f3c82b45711549d36026f72ab92b27e
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-07-08 21:55:24 +0300

    appear toaster when user is offline

--- frontend/src/provider/InternetConnectionProvider.tsx
+++ frontend/src/provider/InternetConnectionProvider.tsx
@@ -1,28 +1,55 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { toaster } from "../components/ui/toaster";
 
 const InternetConnectionProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
+  const toastIdRef = useRef<string | null>(null);
   const [isOnline, setIsOnline] = useState(true);
 
+  function close() {
+    toaster.dismiss(toastIdRef.current || "");
+  }
+
+  function addToast() {
+    toastIdRef.current = toaster.create({
+      title: "No Internet Connection",
+      description: "You are currently offline. Please check your connection.",
+      type: "warning",
+      duration: Infinity,
+      closable: true,
+    });
+  }
+
   useEffect(() => {
-    setIsOnline(navigator.onLine);
+    const handleOnline = () => {
+      setIsOnline(true);
+      close();
+    };
+
+    const handleOffline = () => {
+      setIsOnline(false);
+      addToast();
+    };
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    // Check status on initial mount
+    if (!isOnline) {
+      setIsOnline(false);
+      addToast();
+    }
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
   }, []);
 
-  window.addEventListener("online", () => {
-    console.log("Online");
-  });
-
-  window.addEventListener("offline", () => {
-    console.log("Offline");
-  });
-
-  if (!isOnline) {
-    return <>{children}</>;
-  }
-  return { children };
+  return children;
 };
 
 export default InternetConnectionProvider;
